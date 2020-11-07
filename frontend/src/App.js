389e3ac5515e898cfcd558cc8f0c0461ae860710
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-07 10:47:28 +0200

    change the product image size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-
+import "./index.css";
 function App() {
   return (
     <BrowserRouter>
